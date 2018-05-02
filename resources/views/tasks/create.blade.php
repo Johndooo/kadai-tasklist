7cf22514f008f4725fe0362feffa4ec28c75bf8d
Jhondooo <dooo.inc@gmail.com>
2018-05-02 10:50:35 +0000

    CRUD tasks modified

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -4,12 +4,12 @@
 
     <h1>タスクの新規作成</h1>
 
-    {!! Form::model(task, ['route' => 'tasks.store']) !!}
+    {!! Form::model($task, ['route' => 'tasks.store']) !!}
 
         {!! Form::label('content', 'タスク:') !!}
         {!! Form::text('content') !!}
 
-        {!! Form::submit('タスクを登録') !!}
+        {!! Form::submit('タスク登録') !!}
 
     {!! Form::close() !!}
 
