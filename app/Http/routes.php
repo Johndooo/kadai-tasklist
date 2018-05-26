0e7216a9b80a583c3dcd41f8cc6cbef5fb68a529
Jhondooo <dooo.inc@gmail.com>
2018-05-26 02:33:45 +0000

    change toppage routing

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,7 +10,6 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-
-Route::get('/', 'TasksController@index');
-
-Route::resource('tasks', 'TasksController');
+Route::get('/', function () {
+    return view('welcome');
+});
